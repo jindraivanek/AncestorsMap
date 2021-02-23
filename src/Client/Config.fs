76e3414d383bb8456bf850e68393edc81ef503b1
jindraivanek <jindraivanek>
2021-02-23 12:04:04 +0100

    Move title and extra footer to Config.fs

---
+++ src/Client/Config.fs
@@ -0,0 +1,6 @@
+﻿module Config
+open Fable.React
+
+let title = "Historic collections map"
+
+let footer = div [] [str "Model modified by Markéta Ivánková, Institute of Scientific Infromation, First Faculty of Medicine, Charles University, 2020"]
